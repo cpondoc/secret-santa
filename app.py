d29c9b5fe08649477baf8eb135a1f770268b4a4b
Chris Pondoc <chris.pondoc@comcast.net>
2020-12-06 21:59:42 -0500

    Added makeshift database

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template, redirect, url_for, send_from_directory
-from main import create_dict
+from main import create_dict, parse_document
 app = Flask(__name__)
 
 def create_generation(names):
-    pairings = create_dict(names, names .copy())
+    pairings = create_dict(names, names.copy())
     print(pairings)
     return pairings
 
-names = ['Chris', 'Alyssa', 'Aidan', 'Summer', "Miguel", "Alex", "Caesar"]
-pairings = create_generation(names)
+test = parse_document()
+print(test)
+
+#names = ['Chris', 'Alyssa', 'Aidan', 'Summer', "Miguel", "Alex", "Caesar"]
+#pairings = create_generation(names)
 
 @app.route('/')
 def hello_world():
-    global pairings
     names = ['Chris', 'Alyssa', 'Aidan', 'Summer', "Miguel", "Alex", "Caesar"]
-    duplicateNames = names.copy()
-    return render_template('index.html', names=duplicateNames)
+    return render_template('index.html', names=names)
 
 @app.route('/favicon.ico')
 def favicon():
@@ -24,12 +25,12 @@ def favicon():
 
 @app.route('/generate')
 def generate_new():
-    global pairings
+    global test
     names = ['Chris', 'Alyssa', 'Aidan', 'Summer', "Miguel", "Alex", "Caesar"]
-    pairings = create_generation(names)
+    test = create_dict(names, names.copy())
     return redirect('/')
 
 @app.route('/match/<person>')
 def find_match(person):
-    names = ['Chris', 'Alyssa', 'Aidan', 'Summer', "Miguel", "Alex", "Caesar"]
-    return render_template('match.html', person=person.capitalize(), match=pairings[person.capitalize()])
+    global test
+    return render_template('match.html', person=person.capitalize(), match=test[person.capitalize()])
--- main.py
+++ main.py
@@ -1,18 +1,67 @@
 # Importing libraries
 import random
 import time
+import csv
 
+# Parsing the document
+def parse_document():
+
+    # Opening up the database
+    with open('database.csv') as csv_file:
+
+        # Configuring the dictionary and reader
+        pairings = {}
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+
+        # Parsing through each line, adding to dictionary
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                pairings[row[0]] = row[1]
+                line_count += 1
+        
+        # Returning the correct # of pairings
+        return pairings
+
+# Creating the dictionary and saving to the
 def create_dict(names, duplicateNames):
+    
+    # Seeding and setting up dictionary
     random.seed(time.time())
     pairings = {}
+
+    # Parsing through the list of names
     while (len(names) and len(duplicateNames)):
+
+        # Creating random pair and person
         person = random.choice(names)
         pair = random.choice(duplicateNames)
+
+        # Making sure pair and person are not same
         while (person == pair and len(names) != 1):
             print(person + ", " + pair)
             person = random.choice(names)
             pair = random.choice(duplicateNames)
+
+        # Updating dictionary, removing respective names
         pairings[person] = pair
         names.remove(person)
         duplicateNames.remove(pair)
+    
+    # Saving to makeshift database
+    with open('database.csv', mode='w') as database_file:
+        database_writer = csv.writer(database_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        # Header
+        database_writer.writerow(['Person', 'Match'])
+        
+        for key in pairings.keys():
+            database_writer.writerow([key, pairings[key]])
+
     return pairings
+
+
+names = ['Chris', 'Alyssa', 'Aidan', 'Summer', "Miguel", "Alex", "Caesar"]
+create_dict(names, names.copy())
